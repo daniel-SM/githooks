d4e7cc114be62846339182c23964245d5384123b
Daniel Santos <daniels11.dev@gmail.com>
2019-10-16 13:58:52 -0300

    Repositories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(async() => {
+    const response = await fetch('https://api.ggithub.com/users/diego3g');
+    const data = response.json();
+
+    setRepositories(data);
+  }, []);
+
   return (
-    <div className="App">
-      
-    </div>
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>{repo.name}</li>
+      ))}
+    </ul>
   );
 }
 
